a8dacfd463fb9fe67fa58c4551d9fed54b66904f
julscode <jcah549@gmail.com>
2024-03-12 14:47:28 -0600

    first commit

---
+++ src/app.py
@@ -0,0 +1,71 @@
+from flask import Flask ,render_template,request,redirect,url_for
+import os
+import database as db
+
+template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+
+template_dir= os.path.join(template_dir,'src','templates')
+
+app = Flask(__name__,template_folder = template_dir)
+
+#rutas de la apliaccion 
+
+@app.route('/')
+def home():
+    cursor = db.database.cursor()
+    cursor.execute("select * from users")
+    myresult = cursor.fetchall()
+    #convertir los datos a diccionario 
+    insertObject = []
+    columnNames = [column[0] for column in cursor.description]
+    for record in myresult:
+        insertObject.append(dict(zip(columnNames,record)))
+
+    cursor.close()
+    return render_template('index.html',data = insertObject)
+
+#ruta para guardar usuarios en la bd
+@app.route('/user',methods=['POST'])
+def adduser():
+    username = request.form['username']
+    name = request.form['name']
+    password = request.form['password']
+
+    if username and name and password:
+        cursor =db.database.cursor()
+        sql= "INSERT INTO users(username,name,password) VALUES(%s,%s,%s)"
+        data = (username,name,password)
+        cursor.execute(sql,data)
+        db.database.commit()
+        return redirect(url_for('home'))    
+
+@app.route('/delete/<string:id>')
+def delete(id):
+        cursor =db.database.cursor()
+        sql= "DELETE FROM  users WHERE id = %s"
+        data = (id,)
+        cursor.execute(sql,data)
+        db.database.commit()
+
+        return redirect(url_for('home'))    
+
+@app.route('/edit/<string:id>', methods=['POST'])
+def edit(id):
+     username = request.form['username']
+     name = request.form['name']
+     password = request.form['password']
+
+     if username and name and password:
+          cursor = db.database.cursor()
+          sql = "UPDATE users SET username =%s ,name=%s,password=%s WHERE id= %s"
+          data = (username,name,password,id)
+          cursor.execute(sql,data)
+          db.database.commit()
+          return redirect(url_for('home'))  
+     
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=4000)
+
+
---
+++ src/database.py
@@ -0,0 +1,7 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    user='julscode',
+    password='Montana2204*',
+    database='company'
+)
